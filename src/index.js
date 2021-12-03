ece36b6ed1facae18436f6b3d10ed402c9d2c517
Brendon Ngirazi <48953676+Ju99ernaut@users.noreply.github.com>
2021-12-03 11:43:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,12 +47,6 @@ export default (editor, opts = {}) => {
 
   // TODO Remove
   editor.on('load', () => {
-    editor.addComponents(
-      `<div style="margin:100px; padding:25px;">
-            Content loaded from the plugin
-        </div>`,
-      { at: 0 }
-    );
     appendTailwindCss(editor);
   });
 };
